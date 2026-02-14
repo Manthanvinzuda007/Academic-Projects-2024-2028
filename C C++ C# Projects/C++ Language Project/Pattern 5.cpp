b2592951425420725affc6c528d564c9ff569e77
Manthan Vinzuda <206708551+Manthanvinzuda007@users.noreply.github.com>
2026-02-15 00:34:11 +0530

    Update Pattern 5.cpp

--- C C++ C# Projects/C++ Language Project/Pattern 5.cpp
+++ C C++ C# Projects/C++ Language Project/Pattern 5.cpp
@@ -1,1 +1,20 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; i++) {
+        // First Inner Loop: Prints the spaces to push stars to the right
+        for (int j = 1; j <= rows - i; j++) {
+            cout << "  "; 
+        }
+        // Second Inner Loop: Prints the stars
+        for (int k = 1; k <= i; k++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
